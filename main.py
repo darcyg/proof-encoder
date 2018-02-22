fae9d727d836e20b8276d009e5f0cbb959aa2788
Joe Wood <joe.wood@bbc.co.uk>
2018-02-22 21:54:54 +0000

    Working encode for all ffmpeg options apart from -cqp and -tff

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ ffmbcaudio2jfe_sd = {"validFor": "0x0","ffmbc": "-threads 4 -f image2 -loop 1 -s
 ffmbcnoarc_hd = {"validFor": "1440x1080|1280x720|1200x676|1216x688|1920x1080","ffmbc": "-threads 4 -i %INPUT% -r 25 -ar 48000 -vf scale=1440:1080:interl=1 -vcodec libx264 -cqp 18 -profile high -preset fast -tff -flags +loop+ildct+ilme -acodec mp2 -ab 192k -aspect 16:9 -f mpegts -y -ac 2 %OUTPUT%"}
 ffmbcnoarc_sd = {"validFor": "480x272|480x270|400x222|576x320|568x320|640x360|640x368|640x352|848x480|960x540|1024x576|720x576|720x406|720x404|960x544","ffmbc": "-threads 4 -i %INPUT% -r 25 -ar 48000 -vf scale=702:576:interl=1,pad=720:576:8:0:black -vcodec libx264 -cqp 18 -profile high -preset fast -tff -flags +loop+ildct+ilme -acodec mp2 -ab 192k -aspect 16:9 -f mpegts -y -ac 2 %OUTPUT%"}
 #getting unrecognised uption -cqp, -tff
-ffmbcnoarc_hd_copy = {"validFor": "1440x1080|1280x720|1200x676|1216x688|1920x1080","ffmbc": ['-threads 4 -i pipe:0 -r 25 -ar 48000', '-vf "scale=1440:1080:interl=1"', "-vcodec libx264 -profile high -preset fast -flags +loop+ildct+ilme -acodec mp2 -ab 192k -aspect 16:9 -f mpegts -y -ac 2 pipe:1"]}
-# ffmbcnoarc_hd_copy = {"validFor": "1440x1080|1280x720|1200x676|1216x688|1920x1080","ffmbc": [ '-vf "scale=1440:1080:interl=1"', "-vcodec libx264 -profile high -preset fast -flags +loop+ildct+ilme -acodec mp2 -ab 192k -aspect 16:9 -f mpegts -y -ac 2 pipe:1"]}
+
+ffmbcnoarc_hd_copy = {"validFor": "1440x1080|1280x720|1200x676|1216x688|1920x1080","ffmbc": '-threads 4 -r 25 -ar 48000 -vf "scale=1440:1080:interl=1" -vcodec libx264  -profile high -preset fast -flags +loop+ildct+ilme -acodec mp2 -ab 192k -aspect 16:9 -f mpegts -y -ac 2'}
 
 # mp4 encoding only seems to work if i move the flags with this account
 #  ffmpeg -i input.mp4 -c copy -movflags +faststart output.mp4. This moves MOOV Atom to the beginning of the file.
+# This only matters when piping in the video setting the video directly as the input works fine. =)
 ff = ffmpy.FFmpeg(
     inputs={'test_video.mp4': None},
-    outputs={'pipe:1':  '-vf "scale=1440:1080:interl=1" -c:v h264 -f mpegts -y'}
+    outputs={'pipe:1':  ffmbcnoarc_hd_copy.get('ffmbc')}
 )
 
 print(ff.cmd)
